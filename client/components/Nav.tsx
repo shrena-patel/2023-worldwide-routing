b8bcea6b8dd9eedffa0bd499c481b807b4431283
Shrena Patel <shrena.p@hotmail.co.uk>
2023-08-16 09:58:58 +1200

    step 1 and 2 done

--- client/components/Nav.tsx
+++ client/components/Nav.tsx
@@ -1,9 +1,21 @@
+import continentData from '../../data/continents'
+import Home from './Home'
+
 function Nav() {
+  const continentNames = Object.keys(continentData)
+  console.log(continentNames)
   return (
     <div>
       <h2>Nav</h2>
       <ul>
-        <li>Build your continent list here</li>
+        {continentNames.map((continent) => {
+          return (
+            <>
+              <li key={continent}>{continent}</li>
+            </>
+          )
+        })}
+        <li>Home</li>
       </ul>
     </div>
   )
