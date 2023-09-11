0c70291a77ad4a2dac1d0ef3dff5b0b4f0383755
Shrena Patel <shrena.p@hotmail.co.uk>
2023-09-11 18:34:19 +1200

    first test done

--- client/components/__tests__/Continent.test.tsx
+++ client/components/__tests__/Continent.test.tsx
@@ -4,13 +4,10 @@ import { renderApp } from '../../test-setup'
 
 describe('<Continent/>', () => {
   it('Displays the Africa continent image', async () => {
-    const {  ...screen } = renderApp('/continents/Africa')
-
-    const img = await screen.getAllByAltText('Africa-continent')
-    console.log('img', img)
-    screen.debug()
-    expect (img).toHaveAttribute('src', '/images/South-Africa.jpg')
+    const { ...screen } = renderApp('/continents/Africa')
+    const img = screen.getByAltText('Africa-continent')
+    expect(img).toHaveAttribute('src', '/images/South-Africa.jpg')
   })
 })
 
-// might need to use findByRole instead of getByRole that Gerard uses
+// might need to use findByRole instead of getByRole that is shown in video
